7bd6faa7a3388e202415bd5b068af9137195b695
BakaCow <1jyerichardson1@gmail.com>
2015-12-11 17:07:43 +1100

    Update theme.js

--- theme.js
+++ theme.js
@@ -1,1 +1,1 @@
-$('head').append('<link rel="stylesheet" href="" type="text/css"/>');
+$('head').append('<link rel="stylesheet" href="https://github.com/BakaCow/theme/blob/master/theme.css" type="text/css"/>');

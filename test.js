cc8b1eec8d862e4eefb95426325def85c56f8081
Elitehunter47 <ferminuno@gmail.com>
2015-12-20 05:51:29 +0100

    Delete test.js

--- test.js
+++
@@ -1,8 +0,0 @@
-function start() {
-$('head').append('<link id="KawaiiThemeV2" rel="stylesheet" href="https://rawgit.com/Elitehunter47/theme/master/theme.css">');
-
-$('body').append('<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>');
-$('body').append('<script src="jquery-sakura.min.js"></script>');
-$('head').append('<link href="jquery-sakura.css" rel="stylesheet" type="text/css">');
-$('body').append('<script> $(window).load(function () { $('body').sakura(); }); </script>');
-}

4df8215f0816c87c1e24aff6de648c0d77b38e7d
Elitehunter47 <ferminuno@gmail.com>
2015-12-26 04:10:40 +0100

    Update monochrome.js

--- Monochrome/monochrome.js
+++ Monochrome/monochrome.js
@@ -5,6 +5,5 @@ rndimg = new Array("http://i.imgur.com/2pEEFLQ.gif", "https://alpha.nightcore-33
 x=(Math.floor(Math.random()*numimages));
 randomimage=(rndimg[x]);
 document.getElementById("room-title").style.content = "url("+ randomimage +")";
+$('head').append('<link rel="stylesheet" href="https://rawgit.com/Elitehunter47/theme/master/Monochrome/test.css" type="text/css" />');
 }
-
-if (!document.getElementById) document.write('<link rel="stylesheet" type="text/css" href="https://rawgit.com/Elitehunter47/theme/master/Monochrome/test.css">');
